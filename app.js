c08e869c4041d37facb12c826ba19a97368b26a7
itsbohara <13344241+itsbohara@users.noreply.github.com>
2021-06-15 17:39:41 +0545

    fixed direct url routes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require("express");
 const dotenv = require("dotenv");
 var cookieParser = require("cookie-parser");
@@ -24,6 +26,11 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("frontend/build"));
 }
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './frontend/build/', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`backend is run on server ${PORT}`);
